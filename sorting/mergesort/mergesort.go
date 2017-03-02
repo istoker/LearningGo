2ae5886ee717bc6f83feca7f4588aeb439ac0acd
Idzard Stoker <stoker.idzard+github@gmail.com>
2017-03-02 08:11:23 +0100

    Implemented basic mergesort function
    Implemented a basic mergesort function for sorting arrays filled with integers. Tests must still be written.

---
+++ sorting/mergesort/mergesort.go
@@ -0,0 +1,45 @@
+package mergesort
+
+//Merges the two sorted subslices, which are indicated by half.
+func merge(arr []int, half int){
+    lowSlice := make([]int, len(arr[:half]))
+    copy(lowSlice, arr[:half])
+    highSlice := make([]int, len(arr[half:]))
+    copy(highSlice, arr[half:])
+    i := 0;
+    j := 0;
+    k := 0;
+    for i < len(lowSlice) && j < len(highSlice){
+        if lowSlice[i] <= highSlice[j]{
+            arr[k] = lowSlice[i]
+            i++
+        } else{
+            arr[k] = highSlice[j]
+            j++
+        }
+        k++
+    }
+    if i < len(lowSlice){
+        for i < len(lowSlice){
+            arr[k] = lowSlice[i]
+            i++
+            k++
+        }
+    } else{
+        for j < len(highSlice){
+            arr[k] = highSlice[j]
+            j++
+            k++
+        }
+    }
+}
+
+//MergeSort sorts the array using the mergesort algortihm
+func MergeSort(arr []int){
+    if(len(arr) > 1){
+        half := len(arr)/2
+        MergeSort(arr[:half])
+        MergeSort(arr[half:])
+        merge(arr, half)
+    }
+}
--- sorting/sortmain.go
+++ sorting/sortmain.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "github.com/istoker/LearningGO/sorting/selsort"
 import "github.com/istoker/LearningGo/sorting/inssort"
+import "github.com/istoker/LearningGO/sorting/mergesort"
 
 func main(){
     arr := [20]int{19,18,17,16,14,15,13,12,10,9,11,8,7,4,5,6,3,2,1,0}
@@ -10,10 +11,14 @@ func main(){
     fmt.Println(arr)
     selSortArr := arr
     insSortArr := arr
+    merSortArr := arr
     selsort.SelectionSort(selSortArr[:])
     inssort.InsertionSort(insSortArr[:])
+    mergesort.MergeSort(merSortArr[:])
     fmt.Println("Array sorted with selection sort:")
     fmt.Println(selSortArr)
     fmt.Println("Array sorted with insertion sort:")
     fmt.Println(insSortArr)
+    fmt.Println("Array sorted with merge sort:")
+    fmt.Println(merSortArr)
 }
